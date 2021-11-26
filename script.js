7f62b74b3216f6b94aa82195b468cb7973d78249
Louis <louis.m.newton@hotmail.fr>
2021-11-26 23:18:13 +0100

    fonctionnement de bouton M+ qui ajoute le résultat au résultats précédemment enregistrés
    utilisation du localstorage

--- script.js
+++ script.js
@@ -11,39 +11,48 @@ window.onload = () => {
     for (let touche of touches) {
         touche.addEventListener("click", gererTouches);
     }
+
+    memoire = (localStorage.memoire) ? parseFloat(localStorage.memoire) : 0;
+    if (memoire != 0) memoireElt.style.display = "initial";
 }
 
 function gererTouches() {
     let strValeur = this.innerText;
 
-    if(parseFloat(strValeur)>=0 || strValeur ==="."){
-        affichage = (affichage === "" ) ? strValeur.toString() : affichage + strValeur.toString();
+    if (parseFloat(strValeur) >= 0 || strValeur === ".") {
+        affichage = (affichage === "") ? strValeur.toString() : affichage + strValeur.toString();
         ecranElt.innerText = affichage;
     }
-    else{
-        switch(strValeur){
+    else {
+        switch (strValeur) {
             case "C":
                 precedent = 0;
                 affichage = "";
                 operation = null;
                 ecranElt.innerText = 0;
                 break;
-            
+
             case "+":
             case "-":
             case "*":
-                case "/":
-                    precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
-                    ecranElt.innerText = precedent;
-                    operation = strValeur;
-                    affichage = "";
-                    break;  
-                case "=":
+            case "/":
+                precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
+                ecranElt.innerText = precedent;
+                operation = strValeur;
+                affichage = "";
+                break;
+
+            case "=":
                 precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
                 ecranElt.innerText = precedent;
                 affichage = precedent;
                 precedent = 0;
-                break;    
+                break;
+            
+            case "M+":
+                localStorage.memoire = (localStorage.memoire) ? parseFloat(localStorage.memoire) + parseFloat(affichage) : parseFloat(affichage);
+                memoireElt.style.display = "initial";
+                break;
         }
     }
 }
@@ -55,13 +64,12 @@ function gererTouches() {
  * @param {string} operation
  * @returns float 
  */
-function calculer(nb1, nb2, operation){
+function calculer(nb1, nb2, operation) {
     nb1 = parseFloat(nb1);
     nb2 = parseFloat(nb2);
-    if(operation === "+") return nb1 + nb2;
-    if(operation === "-") return nb1 - nb2;
-    if(operation === "*") return nb1 * nb2;
-    if(operation === "/") return nb1 / nb2;
-
+    if (operation === "+") return nb1 + nb2;
+    if (operation === "-") return nb1 - nb2;
+    if (operation === "*") return nb1 * nb2;
+    if (operation === "/") return nb1 / nb2;
 }
 
