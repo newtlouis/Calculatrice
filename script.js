22300f6988b17a45e4d6fcd9f9e5784e5e08e37b
Louis <louis.m.newton@hotmail.fr>
2021-11-26 23:02:37 +0100

    Fonctionnement des opérations =+-/*
    Stockage du résultat dans variable "precedent"

--- script.js
+++ script.js
@@ -17,8 +17,51 @@ function gererTouches() {
     let strValeur = this.innerText;
 
     if(parseFloat(strValeur)>=0 || strValeur ==="."){
-        affichage = strValeur;
+        affichage = (affichage === "" ) ? strValeur.toString() : affichage + strValeur.toString();
         ecranElt.innerText = affichage;
     }
+    else{
+        switch(strValeur){
+            case "C":
+                precedent = 0;
+                affichage = "";
+                operation = null;
+                ecranElt.innerText = 0;
+                break;
+            
+            case "+":
+            case "-":
+            case "*":
+                case "/":
+                    precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
+                    ecranElt.innerText = precedent;
+                    operation = strValeur;
+                    affichage = "";
+                    break;  
+                case "=":
+                precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
+                ecranElt.innerText = precedent;
+                affichage = precedent;
+                precedent = 0;
+                break;    
+        }
+    }
+}
+
+/**
+ * 
+ * @param {float} nb1 
+ * @param {float} nb2 
+ * @param {string} operation
+ * @returns float 
+ */
+function calculer(nb1, nb2, operation){
+    nb1 = parseFloat(nb1);
+    nb2 = parseFloat(nb2);
+    if(operation === "+") return nb1 + nb2;
+    if(operation === "-") return nb1 - nb2;
+    if(operation === "*") return nb1 * nb2;
+    if(operation === "/") return nb1 / nb2;
+
 }
 
